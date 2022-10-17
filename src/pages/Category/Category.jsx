175fda7ab7ef593312eaebde22449ff75fc6e9bd
GB <voducthang16@gmail.com>
2022-10-17 19:35:02 +0700

    update category UI

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { Link, useParams } from 'react-router-dom';
 import './Category.scss';
 import Product from '~/layouts/components/Product';
@@ -19,21 +19,60 @@ function Category() {
                 </div>
             </div>
             {/* category */}
-            <div className="container grid grid-cols-12 gap-8 mt-10">
-                <div className="col-span-3 bg-slate-200">left</div>
-                <div className="col-span-9">
-                    <div className="grid grid-cols-3 gap-8">
-                        <div className="col-span-1">
-                            <Product />
+            <div className="container">
+                <div className="grid grid-cols-12 gap-8 mt-10">
+                    <div className="col-span-3 ">
+                        <h5 className="text-xl font-semibold pb-4 mb-4 border-b border-slate-300/80">Danh mục</h5>
+                        <ul className="text-base space-y-4">
+                            <li>
+                                <Link className="hover:opacity-60 transition-all" to={'/category/1'}>
+                                    Danh muc 1
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="hover:opacity-60 transition-all" to={'/category/2'}>
+                                    Danh muc 2
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="hover:opacity-60 transition-all" to={'/category/3'}>
+                                    Danh muc 3
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-span-9">
+                        <div className="grid grid-cols-3 gap-8">
+                            <div className="col-span-1">
+                                <Product />
+                            </div>
+                            <div className="col-span-1">
+                                <Product />
+                            </div>
+                            <div className="col-span-1">
+                                <Product />
+                            </div>
+                            <div className="col-span-1">
+                                <Product />
+                            </div>
                         </div>
-                        <div className="col-span-1">
-                            <Product />
+                    </div>
+                </div>
+            </div>
+            {/* pagination */}
+            <div className="container mt-20">
+                <div className="grid grid-cols-12">
+                    <div className="col-span-9 col-start-4 flex justify-center items-center space-x-4">
+                        <div className="pagination-button">
+                            <AiOutlineArrowLeft />
                         </div>
-                        <div className="col-span-1">
-                            <Product />
-                        </div>
-                        <div className="col-span-1">
-                            <Product />
+                        <ul className="flex space-x-2">
+                            <li className="pagination-button active">1</li>
+                            <li className="pagination-button">2</li>
+                            <li className="pagination-button">3</li>
+                        </ul>
+                        <div className="pagination-button">
+                            <AiOutlineArrowRight />
                         </div>
                     </div>
                 </div>
