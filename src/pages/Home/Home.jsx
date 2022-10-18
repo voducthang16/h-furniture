5bf5c7e2c5ad07435abba1dd9cae0e2ca6f18534
nguyennhieu1507 <nhieunxps16310@fpt.edu.vn>
2022-10-18 18:22:26 +0700

    code

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.scss';
+import Banner from '~/layouts/components/Banner';
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <Banner></Banner>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
