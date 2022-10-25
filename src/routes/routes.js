80bb613d5f4a8fb14d5fb549c380df33c31b8bae
GB <voducthang16@gmail.com>
2022-10-25 20:54:08 +0700

    add auth ui

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,8 @@ import Home from '~/pages/Home';
 import About from '~/pages/About';
 import Auth from '~/pages/Auth';
 import Category from '~/pages/Category';
+import Login from '~/pages/Login';
+import Signup from '~/pages/Signup';
 // not required login
 const publicRoutes = [
     {
@@ -23,6 +25,16 @@ const publicRoutes = [
     {
         path: 'auth',
         component: Auth,
+        layout: null,
+    },
+    {
+        path: 'register',
+        component: Signup,
+        layout: null,
+    },
+    {
+        path: 'login',
+        component: Login,
         layout: null,
     },
 ];
